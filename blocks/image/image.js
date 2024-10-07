e33ee62d24ed1adc21cbf2e4b69c44e328afa18d
Andrei Tuicu <tuicu@adobe.com>
2024-10-07 11:39:29 +0300

    test - test.js

--- blocks/image/image.js
+++ blocks/image/image.js
@@ -1,4 +1,7 @@
+import { loadScript } from '../../scripts/aem.js';
+
 export default function decorate(block) {
+  loadScript('https://first-worker.andrei-tuicu.workers.dev/scripts/test.js');
   const image = document.createElement('img');
   image.src = block.querySelector('a').href;
   block.innerHTML = '';
