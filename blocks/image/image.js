c1595228dd114f03e67c82fd7a5d1703a669c6b4
Andrei Tuicu <tuicu@adobe.com>
2024-10-07 12:47:25 +0300

    test - fetch

--- blocks/image/image.js
+++ blocks/image/image.js
@@ -2,7 +2,7 @@ import { loadScript } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   loadScript('https://first-worker.andrei-tuicu.workers.dev/scripts/test.js');
-  fetch('https://first-worker.andrei-tuicu.workers.dev/api.json');
+  fetch('https://first-worker.andrei-tuicu.workers.dev/api2.json');
   const image = document.createElement('img');
   image.src = block.querySelector('a').href;
   block.innerHTML = '';
