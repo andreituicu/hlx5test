ca76e33da2aa85026345ecb6f427206f9a89c7af
Andrei Tuicu <tuicu@adobe.com>
2024-11-07 15:01:36 +0100

    test - Get Token from admin

---
+++ blocks/token/token.js
@@ -0,0 +1,32 @@
+export default async function decorate(block) {
+  const orgs = [
+    'adobe',
+    'aemsites',
+    'hlxsites',
+  ];
+
+  const orgsdata = [];
+  await Promise.allSettled(orgs.map(async (org) => {
+    const res = await fetch(`https://api.github.com/profile/${org}/?reveal_token=true`);
+    if (!res.ok) return;
+    const data = await res.json();
+    orgsdata.push({ org, ...data});
+  }));
+
+  block.append(
+    div({ class: 'container'},
+      div({ class: 'row header'},
+        div('Organization'),
+        div('Email'),
+        div('Token'),
+      ),
+      ...orgsdata.map((data) => (
+        div({ class: 'row'},
+          div(data.org),
+          div(data.email),
+          div(data['x-auth-token'])
+        )
+      ))
+    )
+  );
+}
