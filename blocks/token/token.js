6c2ac040fe03cdcb0a582e90db496cbb978ad3f1
Andrei Tuicu <tuicu@adobe.com>
2024-11-08 13:18:15 +0100

    test

--- blocks/token/token.js
+++ blocks/token/token.js
@@ -15,35 +15,35 @@ export default async function decorate(block) {
     'stericycle',
   ];
 
-  const resp = await fetch('https://admin.hlx.page/config/andreituicu/sites.json', { method: "GET", credentials: "include" });
-  if (!resp.ok) {
-    console.log(resp.statusText);
-  } else {
-    console.log(await resp.json());
-  }
+  // const resp = await fetch('https://admin.hlx.page/config/andreituicu/sites.json', { method: "GET", credentials: "include" });
+  // if (!resp.ok) {
+  //   console.log(resp.statusText);
+  // } else {
+  //   console.log(await resp.json());
+  // }
 
-  // const orgsdata = [];
-  // await Promise.allSettled(orgs.map(async (org) => {
-  //   const res = await fetch(`https://admin.hlx.page/profile/${org}/?reveal_token=true`);
-  //   if (!res.ok) return;
-  //   const data = await res.json();
-  //   orgsdata.push({ org, ...data});
-  // }));
+  const orgsdata = [];
+  await Promise.allSettled(orgs.map(async (org) => {
+    const res = await fetch(`https://admin.hlx.page/profile/${org}/?reveal_token=true`, { method: "GET", credentials: "include" });
+    if (!res.ok) return;
+    const data = await res.json();
+    orgsdata.push({ org, ...data});
+  }));
 
-  // block.append(
-  //   div({ class: 'container'},
-  //     div({ class: 'row header'},
-  //       div('Organization'),
-  //       div('Email'),
-  //       div('Token'),
-  //     ),
-  //     ...orgsdata.map((data) => (
-  //       div({ class: 'row'},
-  //         div(data.org),
-  //         div(data.profile?.email),
-  //         div(displayToken(data.profile?.['x-auth-token'])),
-  //       )
-  //     ))
-  //   )
-  // );
+  block.append(
+    div({ class: 'container'},
+      div({ class: 'row header'},
+        div('Organization'),
+        div('Email'),
+        div('Token'),
+      ),
+      ...orgsdata.map((data) => (
+        div({ class: 'row'},
+          div(data.org),
+          div(data.profile?.email),
+          div(displayToken(data.profile?.['x-auth-token'])),
+        )
+      ))
+    )
+  );
 }
