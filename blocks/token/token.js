b2a1f0b5fd840a663d1a9e4d4478a0a673fcaaf5
Andrei Tuicu <tuicu@adobe.com>
2024-11-08 13:15:22 +0100

    test

--- blocks/token/token.js
+++ blocks/token/token.js
@@ -22,28 +22,28 @@ export default async function decorate(block) {
     console.log(await resp.json());
   }
 
-  const orgsdata = [];
-  await Promise.allSettled(orgs.map(async (org) => {
-    const res = await fetch(`https://admin.hlx.page/profile/${org}/?reveal_token=true`);
-    if (!res.ok) return;
-    const data = await res.json();
-    orgsdata.push({ org, ...data});
-  }));
+  // const orgsdata = [];
+  // await Promise.allSettled(orgs.map(async (org) => {
+  //   const res = await fetch(`https://admin.hlx.page/profile/${org}/?reveal_token=true`);
+  //   if (!res.ok) return;
+  //   const data = await res.json();
+  //   orgsdata.push({ org, ...data});
+  // }));
 
-  block.append(
-    div({ class: 'container'},
-      div({ class: 'row header'},
-        div('Organization'),
-        div('Email'),
-        div('Token'),
-      ),
-      ...orgsdata.map((data) => (
-        div({ class: 'row'},
-          div(data.org),
-          div(data.profile?.email),
-          div(displayToken(data.profile?.['x-auth-token'])),
-        )
-      ))
-    )
-  );
+  // block.append(
+  //   div({ class: 'container'},
+  //     div({ class: 'row header'},
+  //       div('Organization'),
+  //       div('Email'),
+  //       div('Token'),
+  //     ),
+  //     ...orgsdata.map((data) => (
+  //       div({ class: 'row'},
+  //         div(data.org),
+  //         div(data.profile?.email),
+  //         div(displayToken(data.profile?.['x-auth-token'])),
+  //       )
+  //     ))
+  //   )
+  // );
 }
