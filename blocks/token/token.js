1466472739f3d30236761d0668538c76ca28ceab
Andrei Tuicu <tuicu@adobe.com>
2024-11-08 13:35:16 +0100

    test

--- blocks/token/token.js
+++ blocks/token/token.js
@@ -10,7 +10,7 @@ function displayToken(token) {
 export default async function decorate(block) {
   const res = await fetch(`https://admin.hlx.page/profile/?reveal_token=true`, { method: "GET", credentials: "include" });
   if (!res.ok) {
-    block.append(div({ class: 'error' }, 'You are not logged into Helix Admin'));
+    block.append(div({ class: 'error' }, 'You are not logged into Helix Admin or you don\'t have third party cookies enabled.'));
   }
 
   const data = await res.json(); 
