e09cbace5e42eec50d2808cdfb5756e52fbd5527
Andrei Tuicu <tuicu@adobe.com>
2024-11-13 18:36:49 +0100

    test

--- blocks/token/token.js
+++ blocks/token/token.js
@@ -12,6 +12,32 @@ export default async function decorate(block) {
     console.log('message', message);
   });
 
+  const sidekickId = 'igkmdomcgoebiipaifhmpfjhbjccggml';
+  const msg = {
+    action: 'getAuthInfo',
+  };
+
+  function handler(resp) {
+    console.log('got response from sidekick:', resp);
+    window.close();
+  }
+
+  if (window.browser) {
+    browser.runtime.sendMessage(sidekickId, JSON.stringify(msg))
+      .then(handler)
+      .catch((err) => {
+        console.error('error invoking extension:', err);
+      });
+  } else {
+    chrome.runtime.sendMessage(sidekickId, JSON.stringify(msg), (resp) => {
+      if (resp) {
+        handler(resp);
+      } else {
+        console.error('error invoking extension:', chrome.runtime.lastError);
+      }
+    });
+  }
+
   // const orgs = [
   //   'adobe',
   //   'aemsites',
