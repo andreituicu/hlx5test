ecfbf9fc2b0e853f6d88771cc2c80f3937bc8a6a
Andrei Tuicu <tuicu@adobe.com>
2024-12-11 10:02:22 +0100

    test - serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -10,10 +10,11 @@ self.addEventListener('activate', event => {
 
 // Fetch event
 self.addEventListener('fetch', event => {
-  console.log('fetch event:', event);
-  const headers = [];
-  for (const [key, value] of event.request.headers.entries()) {
-    headers.push(`${key}: ${value}`);
-  }
-  console.log(headers);
+  fetch(event.request).then(response => {
+    console.log('fetch event:', event);
+    const headers = [];
+    for (const [key, value] of event.request.headers.entries()) {
+      headers.push(`${key}: ${value}`);
+    }
+  });
 });
