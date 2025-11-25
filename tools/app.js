16000754b752dd1787958970466f046ee682fc3e
Andrei Tuicu <tuicu@adobe.com>
2025-11-25 20:41:15 +0100

    test

--- tools/app.js
+++ tools/app.js
@@ -1,7 +1,7 @@
 import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 
 (async function init() {
-  const { context, token } = await DA_SDK;
+  const { token } = await DA_SDK;
 
   alert("Hello! Your token is: " + token.substring(0, 20) + "...");
 }());
